484000ab58f67d15a62683e8f098882f57797b8d
Jon M <jon@mailosaur.com>
2022-10-13 11:16:13 +0100

    Add support for message direction

--- lib/models/index.d.ts
+++ lib/models/index.d.ts
@@ -328,6 +328,10 @@ export interface SearchOptions {
    * When using the 'get' method, this option can be used to prevent an error being thrown if no matching message is found in time.
    */
   suppressError?: boolean
+  /**
+   * Optionally limits results based on the direction (`Sent` or `Received`), with the default being `Received`.
+   */
+  dir?: string
 }
 
 /**
@@ -346,6 +350,10 @@ export interface MessageListOptions {
    * A limit on the number of results to be returned. This can be set between `1` and `1000`, with the default being `50`.
    */
   itemsPerPage?: number
+  /**
+   * Optionally limits results based on the direction (`Sent` or `Received`), with the default being `Received`.
+   */
+  dir?: string
 }
 
 /**
@@ -486,6 +494,10 @@ export interface SearchOptions {
    * When using the 'get' method, this option can be used to prevent an error being thrown if no matching message is found in time.
    */
   suppressError?: boolean
+  /**
+   * Optionally limits results based on the direction (`Sent` or `Received`), with the default being `Received`.
+   */
+  dir?: string
 }
 
 /**
