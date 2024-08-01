e2f14a3ebe57765511467d8097fc620eba6a9db4
Jon M <jon@mailosaur.com>
2024-08-01 12:54:10 +0100

    Update base URL for 'Unauthorized' test

--- test/errors.js
+++ test/errors.js
@@ -2,8 +2,11 @@ const { assert } = require('chai');
 const MailosaurClient = require('../lib/mailosaur');
 
 describe('error handling', () => {
+  const apiKey = process.env.MAILOSAUR_API_KEY;
+  const baseUrl = process.env.MAILOSAUR_BASE_URL || 'https://mailosaur.com/';
+
   it('Unauthorized', (done) => {
-    const client = new MailosaurClient('invalid_key', process.env.MAILOSAUR_API_KEY);
+    const client = new MailosaurClient('invalid_key', baseUrl);
     client.servers.list().catch((err) => {
       assert.equal(err.toString(), 'Error: Authentication failed, check your API key.');
       done();
@@ -11,7 +14,7 @@ describe('error handling', () => {
   });
 
   it('Not Found', (done) => {
-    const client = new MailosaurClient(process.env.MAILOSAUR_API_KEY);
+    const client = new MailosaurClient(apiKey, baseUrl);
     client.servers.get('not_found').catch((err) => {
       assert.equal(err.toString(), 'Error: Not found, check input parameters.');
       done();
@@ -19,7 +22,7 @@ describe('error handling', () => {
   });
 
   it('Bad Request', (done) => {
-    const client = new MailosaurClient(process.env.MAILOSAUR_API_KEY);
+    const client = new MailosaurClient(apiKey, baseUrl);
     client.servers.create({}).catch((err) => {
       assert.equal(err.toString(), 'Error: (name) Servers need a name\r\n');
       done();
